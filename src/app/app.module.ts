2d882aea19caf5d85a5997f845991ebce5742611
Shivraj-cyber <shivrajsinghbah@gmail.com>
2022-02-13 15:35:24 +0530

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule,AppRoutingModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [AppComponent, HelloComponent, DemoComponent],
   bootstrap: [AppComponent],
